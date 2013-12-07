b0190e0be493436b3199f8e1901785db59f35e27
goodluck.sunlight <goodluck.sunlight@gmail.com>
2013-12-07 15:59:41 +0000

    系统常量

---
+++ radius-invoicing/src/java/com/radius/base/utils/Constants.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.radius.base.utils;
+/**
+ * @author 陈波宁 E-mail:goodluck.sunlight@gmail.com
+ * @version 创建时间：2013-12-7 下午10:59:13
+ * 类说明 系统中使用到的一些常量
+ */
+/**
+ * @author boning.cheng
+ *
+ */
+public interface Constants {
+
+	/**
+	 * 用户编号的开始字母
+	 */
+	public String USER_PREFIX="U";
+	/**
+	 * 以采购开始的一些信息 如采购订单 CG-D
+	 */
+	public String STOCK_PREFIX="CG";//采购
+	/**
+	 * 供应商编号的开始字母
+	 */
+	public String SUPPLIER_PREFIX="G";//供运商
+	/**
+	 * 时间格式yyyyMMdd
+	 */
+	public String DATEFORMAT_YEAR_MONTH_DAY="yyyyMMdd";//yyyyMMdd的时间格式
+	
+}
