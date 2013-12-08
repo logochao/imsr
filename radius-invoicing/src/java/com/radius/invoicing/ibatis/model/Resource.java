8842cc810ef9baa37c9484a1bdaf883fa8092e6c
goodluck.sunlight <goodluck.sunlight@gmail.com>
2013-12-08 04:37:04 +0000

    添加 resouce类与对应的SqlMap文件

---
+++ radius-invoicing/src/java/com/radius/invoicing/ibatis/model/Resource.java
@@ -0,0 +1,10 @@
+package com.radius.invoicing.ibatis.model;
+/**
+ * @author 陈波宁 E-mail:goodluck.sunlight@gmail.com
+ * @version 创建时间：2013-12-8 下午12:24:36
+ * 类说明 数据库资源类型
+ */
+public class Resource {
+
+	
+}
