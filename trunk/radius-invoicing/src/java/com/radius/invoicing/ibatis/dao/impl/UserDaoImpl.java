af2656353dec95f5ebbfb4a67579ff79785fb02d
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2013-12-09 15:39:03 +0000

    添加资源DAO层

---
+++ trunk/radius-invoicing/src/java/com/radius/invoicing/ibatis/dao/impl/UserDaoImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.radius.invoicing.ibatis.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.radius.base.dao.ibatis.BaseIbatisDaoImpl;
+
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明
+ */
+@Repository
+public class UserDaoImpl extends BaseIbatisDaoImpl {
+
+}
