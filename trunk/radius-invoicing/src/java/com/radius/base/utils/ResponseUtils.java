fdb359435672f1503539c867a1f0a1f6767ca866
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2013-12-26 17:30:45 +0000

    添加返回数据对应的对象

---
+++ trunk/radius-invoicing/src/java/com/radius/base/utils/ResponseUtils.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.radius.base.utils;
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明
+ */
+public class ResponseUtils {
+
+	private boolean success=false;//是否成功
+	
+	private String message="";//提示信息
+	
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
