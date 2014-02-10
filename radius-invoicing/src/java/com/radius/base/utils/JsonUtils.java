9ebc2fb38d60a8e09a2641913bf115785c06e2f2
goodluck.sunlight <goodluck.sunlight@gmail.com>
2014-02-10 07:52:32 +0000

    重构返回数据对象

--- radius-invoicing/src/java/com/radius/base/utils/JsonUtils.java
+++ radius-invoicing/src/java/com/radius/base/utils/JsonUtils.java
@@ -8,10 +8,51 @@ package com.radius.base.utils;
  * Copyright (c) 2013 by 陈波宁.<br/>
  * 类说明
  */
-/**
- * @author boning.cheng
- *
- */
-public class JsonUtils {
+public class JsonUtils<E> {
 
+	private boolean success=false;//通讯是否成功
+	
+	private String message="";//信息描述
+	
+	private E child;//带处理的对象
+	
+	public JsonUtils() {
+	}
+	
+	
+	public JsonUtils(boolean success,String message) {
+		this.success=success;
+		this.message=message;
+	}
+	
+
+	public E getChild() {
+		return child;
+	}
+
+	public void setChild(E child) {
+		this.child = child;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	
+	
+	
+	
+	
+	
 }
