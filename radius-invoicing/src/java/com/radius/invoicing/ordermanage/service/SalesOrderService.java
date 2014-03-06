36fae98f6caeb0778d8de076a9ef6c57b1b6ef48
goodluck.sunlight <goodluck.sunlight@gmail.com>
2014-03-06 18:37:04 +0000

    销售订单请求处理与memcached之间的通讯

---
+++ radius-invoicing/src/java/com/radius/invoicing/ordermanage/service/SalesOrderService.java
@@ -0,0 +1,41 @@
+package com.radius.invoicing.ordermanage.service;
+
+import com.radius.base.utils.JsonUtils;
+import com.radius.invoicing.ibatis.model.SalesOrderGoodsGrd;
+
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明
+ */
+public interface SalesOrderService {
+
+	/**
+	 * 将销售订单商品添加到memcached中
+	 * @param key
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils addSalesOrderGoodsGrd2Memcached(String key,
+			SalesOrderGoodsGrd grd);
+
+	/**
+	 * 移除销售订单商品从memcached中
+	 * @param key
+	 * @param isDelete true表示全部删除
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils removeSalesOrderGoodsGrd2Memcached(String key,boolean isDelete,
+			SalesOrderGoodsGrd grd);
+
+	/**
+	 * 保存销售订单信息
+	 * @param objects
+	 * @return
+	 * @throws Exception
+	 */
+	public JsonUtils saveSalesOrderInfos(Object... objects) throws Exception;
+
+}
