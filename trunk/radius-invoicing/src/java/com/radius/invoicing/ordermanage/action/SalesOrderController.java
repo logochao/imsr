fd6c8d0c686dc569701363d3b95b22c8844fb6df
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2014-03-06 18:37:04 +0000

    销售订单请求处理与memcached之间的通讯

--- trunk/radius-invoicing/src/java/com/radius/invoicing/ordermanage/action/SalesOrderController.java
+++ trunk/radius-invoicing/src/java/com/radius/invoicing/ordermanage/action/SalesOrderController.java
@@ -6,14 +6,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.radius.base.controller.BaseController;
+import com.radius.base.utils.JsonUtils;
 import com.radius.base.utils.StockUtils;
 import com.radius.invoicing.ibatis.model.SalesOrder;
+import com.radius.invoicing.ibatis.model.SalesOrderGoodsGrd;
+import com.radius.invoicing.ordermanage.service.SalesOrderService;
 
 /**
  * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
@@ -31,10 +38,15 @@ public class SalesOrderController extends BaseController {
 	
 	private String sales_order_view=null; //销售订单视图
 	
+	@Autowired(required=false)
+	@Qualifier("salesOrderServiceImpl")
+	private SalesOrderService salesOrderService;
+	
+	
 	@PostConstruct
 	public void init(){
 		if(sales_order_view==null){
-			sales_order_view=prefix+"sales_order.jsp";
+			sales_order_view=prefix+"sales_order_index.jsp";
 		}
 	}
 	
@@ -56,10 +68,35 @@ public class SalesOrderController extends BaseController {
 		
 		return mv;
 	}
-	
-	
-	
-	
+	/**
+	 * 将销售订单商品列表添加memcached
+	 * @param request
+	 * @param response
+	 * @param goodsGrd
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/order/manager/salesorder/order_product_info_memcached.html")
+	@ResponseBody
+	public JsonUtils addSalesOrderGoodsGrdInfo(HttpServletRequest request,HttpServletResponse response,SalesOrderGoodsGrd goodsGrd)throws Exception{
+		String key=goodsGrd.getOrderId()+"_sales_order_add_order_product_info";//合同编号_sales_order_add_order_product_info
+		return salesOrderService.addSalesOrderGoodsGrd2Memcached(key, goodsGrd);
+	}
+	/**
+	 * 将memcached中的销售订单对应商品列表移除
+	 * @param request
+	 * @param response
+	 * @param goodsGrd
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/order/manager/salesorder/order_product_info_remove_memcached.html")
+	@ResponseBody
+	public JsonUtils removeSalesOrderGoodsGrdInfo(HttpServletRequest request,HttpServletResponse response,SalesOrderGoodsGrd goodsGrd)throws Exception{
+		String key=goodsGrd.getOrderId()+"_sales_order_add_order_product_info";//合同编号_sales_order_add_order_product_info
+		boolean delete = ServletRequestUtils.getBooleanParameter(request, "delete", false);//表示是否全部删除 默认为 false
+		return salesOrderService.removeSalesOrderGoodsGrd2Memcached(key,delete,goodsGrd);
+	}
 	
 	
 	
---
+++ trunk/radius-invoicing/src/java/com/radius/invoicing/ordermanage/service/impl/SalesOrderServiceImpl.java
@@ -0,0 +1,123 @@
+package com.radius.invoicing.ordermanage.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.radius.base.cache.memcache.MemcacheClient;
+import com.radius.base.utils.Constants;
+import com.radius.base.utils.JsonUtils;
+import com.radius.invoicing.ibatis.model.SalesOrderGoodsGrd;
+import com.radius.invoicing.ordermanage.service.SalesOrderService;
+
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明 销售订单service
+ */
+@Service
+@SuppressWarnings("unchecked")
+public class SalesOrderServiceImpl implements Constants, SalesOrderService{
+
+	private Logger logger=Logger.getLogger(this.getClass());
+	
+	private JsonUtils jsonUtils=null;
+	
+	@PostConstruct
+	public void init(){
+		if(jsonUtils==null){
+			jsonUtils=new JsonUtils();
+		}
+	}
+	
+	/**
+	 * 将销售订单商品添加到memcached中
+	 * @param key
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils addSalesOrderGoodsGrd2Memcached(String key,SalesOrderGoodsGrd grd){
+		Map<String,SalesOrderGoodsGrd> memcache=null;
+		Object cache=MemcacheClient.get(key);
+		String mapKey=grd.getOrderId()+"_"+grd.getGoodsId();//销售订单号_商品编号
+		if(cache!=null){
+			memcache=(Map<String,SalesOrderGoodsGrd>)cache;
+			SalesOrderGoodsGrd goodGrd=memcache.get(mapKey);
+			if(grd!=null){//更新map对象
+				goodGrd.setAmount(grd.getAmount());
+				goodGrd.setQuantity(grd.getQuantity());
+				logger.error("key "+key+",已经将其缓存在对象中 "+cache);
+				grd=goodGrd;
+			}
+		}else{
+			memcache = new HashMap<String, SalesOrderGoodsGrd>();
+		}
+		memcache.put(mapKey, grd);
+		MemcacheClient.set(key, memcache, CACHE_ONE_HOURE_TIME);//将带缓存的对象进行写入
+		return new JsonUtils (true,"将销售订单商品缓存到内存中成功....");
+	}
+	
+	/**
+	 * 移除销售订单商品从memcached中
+	 * @param key
+	 * @param isDelete true表示删除整个对象
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils removeSalesOrderGoodsGrd2Memcached(String key,boolean isDelete,SalesOrderGoodsGrd grd){
+		String memo=grd.getMemo();//该字段存放带删除的主键信息
+		Map<String,SalesOrderGoodsGrd> memcache=null;
+		
+		//默认
+		jsonUtils.setSuccess(false);
+		jsonUtils.setMessage("不存在对应的缓存对象");
+		if(isDelete){//全部删除
+			MemcacheClient.delete(key);
+			jsonUtils.setSuccess(true);
+			jsonUtils.setMessage("删除缓存对象成功...");
+			return jsonUtils;
+		}
+		Object cache=MemcacheClient.get(key);
+		if(cache!=null){
+			String mapKey=null;
+			if(memo.contains(",")){
+				for(String goodsId:memo.split(",")){
+					mapKey=grd.getOrderId()+"_"+goodsId;//销售订单号_商品编号
+					memcache.remove(mapKey);//将key对应的对象进行删除
+				}
+			}else{
+				mapKey=grd.getOrderId()+"_"+memo;//销售订单号_商品编号
+				memcache.remove(mapKey);//将key对应的对象进行删除
+			}
+			jsonUtils.setSuccess(true);
+			jsonUtils.setMessage("删除缓存对象成功...");
+		}
+		MemcacheClient.set(key, memcache, CACHE_ONE_HOURE_TIME);//将带缓存的对象进行写入
+		return jsonUtils;
+	}
+	
+	/**
+	 * 保存销售订单信息
+	 * @param objects
+	 * @return
+	 * @throws Exception
+	 */
+	public JsonUtils saveSalesOrderInfos(Object...objects)throws Exception{
+		
+		
+		return null;
+	}
+	
+	@PreDestroy
+	public void destroy(){
+		if(jsonUtils!=null){
+			jsonUtils=null;
+		}
+	}
+}
