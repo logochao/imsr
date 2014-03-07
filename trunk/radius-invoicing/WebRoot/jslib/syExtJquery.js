71914f5c6f00c48e83b14b165237b344702f6192
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2014-03-07 04:02:45 +0000

    add js sleep

--- trunk/radius-invoicing/WebRoot/jslib/syExtJquery.js
+++ trunk/radius-invoicing/WebRoot/jslib/syExtJquery.js
@@ -278,3 +278,29 @@ sy.autoIframeHeight = function(iframe) {
 sy.setIframeHeight = function(iframe, height) {
 	iframe.height = height;
 };
+
+
+(function($){
+	var _sleeptimer;
+	$.sleep = function( time2sleep, callback )
+	{
+		$.sleep._sleeptimer = time2sleep;
+		$.sleep._cback = callback;
+		$.sleep.timer = setInterval('$.sleep.count()', 100);
+	}
+	$.extend ($.sleep, {
+		current_i : 1,
+		_sleeptimer : 0,
+		_cback : null,
+		timer : null,
+		count : function()
+		{
+			if ( $.sleep.current_i === $.sleep._sleeptimer )
+			{
+				clearInterval($.sleep.timer);
+				$.sleep._cback.call(this);
+			}
+			$.sleep.current_i++;
+		}
+	});
+})(jQuery);
