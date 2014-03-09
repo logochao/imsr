a1869c0b34d9a4cee7c5ad30c1031aed4c7320b4
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2014-03-09 10:57:01 +0000

    添加公用模板的请求处理

---
+++ trunk/radius-invoicing/src/java/com/radius/invoicing/sysmanage/action/CommonController.java
@@ -0,0 +1,152 @@
+/**
+ * 
+ */
+package com.radius.invoicing.sysmanage.action;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.radius.base.helper.PropertyConfigHelper;
+import com.radius.invoicing.cache.impl.CategoryCacheImpl;
+import com.radius.invoicing.ibatis.model.CategoryCode;
+
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明  系统公共调用方法 主要是一个字典函数
+ */
+@Controller
+@Scope("request")
+@SuppressWarnings("static-access")
+public class CommonController {
+
+	@Autowired(required=false)
+	@Qualifier("categoryCacheImpl")
+	private CategoryCacheImpl categoryCache;
+	
+	@Autowired(required=false)
+	@Qualifier("propertyConfigHelper")
+	private PropertyConfigHelper propertyConfigHelper;
+	
+	/**
+	 * 获取分类代码列表
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/common/system/category_code_list.html")
+	@ResponseBody
+	public List<CategoryCode> getCategoryCodeList(HttpServletRequest request,HttpServletResponse response)throws Exception{
+		Integer parentId = ServletRequestUtils.getIntParameter(request, "parentId", 0);//获取参数值
+		return categoryCache.getCategoryCodeListByParentId(parentId);
+	} 
+	
+	/**
+	 * 上传文件
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	@RequestMapping("/common/system/file_upload_list.html")
+	public void fileUploader(HttpServletRequest request,HttpServletResponse response)throws Exception{
+		String repositoryPath = System.getProperty("java.io.tmpdir");
+		String root = request.getSession().getServletContext().getRealPath("/")+"image/";//默认设置为stock下的image文件夹下
+		Object path=propertyConfigHelper.getPropertyValue("contract.image.file.path");
+		String uploadPath=path==null?root:path.toString();
+		File up = new File(uploadPath);
+		if(!up.exists()){
+			up.mkdir();
+		}
+		
+		response.setCharacterEncoding("UTF-8");
+		Integer schunk = null;//分割块数
+		Integer schunks = null;//总分割数
+		String name = null;//文件名
+		BufferedOutputStream outputStream=null; 
+		if (ServletFileUpload.isMultipartContent(request)) {
+			try {
+				DiskFileItemFactory factory = new DiskFileItemFactory();
+				factory.setSizeThreshold(1024);
+				factory.setRepository(new File(repositoryPath));//设置临时目录
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				upload.setHeaderEncoding("UTF-8");
+				upload.setSizeMax(5 * 1024 * 1024);//设置附近大小
+				List<FileItem> items = upload.parseRequest(request);
+				//生成新文件名
+				String newFileName = null;
+				for (FileItem item : items) {
+					if (!item.isFormField()) {// 如果是文件类型
+						name = item.getName();// 获得文件名
+						newFileName = UUID.randomUUID().toString().replace("-","").concat(".").concat(FilenameUtils.getExtension(name));
+						if (name != null) {
+							String nFname = newFileName;
+							if (schunk != null) {
+								nFname = schunk + "_" + name;
+							}
+							File savedFile = new File(uploadPath, nFname);
+							item.write(savedFile);
+						}
+					} else {
+						//判断是否带分割信息
+						if (item.getFieldName().equals("chunk")) {
+							schunk = Integer.parseInt(item.getString());
+						}
+						if (item.getFieldName().equals("chunks")) {
+							schunks = Integer.parseInt(item.getString());
+						}
+					}
+				}
+				
+				if (schunk != null && schunk + 1 == schunks) {
+					outputStream = new BufferedOutputStream(new FileOutputStream(new File(uploadPath, newFileName)));
+					//遍历文件合并
+					for (int i = 0; i < schunks; i++) {
+						File tempFile=new File(uploadPath,i+"_"+name);
+						byte[] bytes=FileUtils.readFileToByteArray(tempFile);  
+						outputStream.write(bytes);
+						outputStream.flush();
+						tempFile.delete();
+					}
+					outputStream.flush();
+				}
+				response.getWriter().write("{\"status\":true,\"newName\":\""+newFileName+"\"}");
+			} catch (FileUploadException e) {
+				e.printStackTrace();
+				response.getWriter().write("{\"status\":false}");
+			} catch (Exception e) {
+				e.printStackTrace();
+				response.getWriter().write("{\"status\":false}");
+			}finally{  
+	            try {  
+	            	if(outputStream!=null)
+	            		outputStream.close();  
+	            } catch (IOException e) {  
+	                e.printStackTrace();  
+	            }  
+	        }   
+		}
+	}
+}
