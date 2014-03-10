f9b06d7a6b1ed7178c5cfc69f7cec431267e3bdd
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2014-03-10 16:53:43 +0000

    添加系统订单相关前缀规则

--- trunk/radius-invoicing/src/java/com/radius/base/utils/Constants.java
+++ trunk/radius-invoicing/src/java/com/radius/base/utils/Constants.java
@@ -18,9 +18,28 @@ public interface Constants {
 	 */
 	public String USER_PREFIX="U";
 	/**
+	 * 库存前缀
+	 */
+	@Deprecated
+	public String STOCK_PREFIX="KC";//库存
+	/**
 	 * 以采购开始的一些信息 如采购订单 CG-D
 	 */
-	public String STOCK_PREFIX="CG";//采购
+	public String PURCHASE_PREFIX="CG";//采购
+	/**
+	 * 以销售开始的一些信息 如销售订单 XS-D
+	 */
+	public String SALES_PREFIX="XS";//销售
+	
+	/**
+	 * 付款单编号前缀 F
+	 */
+	public String PAYMENT_RECEIPT="F";//付款单
+	/**
+	 * 收款单编号前缀 S
+	 */
+	public String RECEIVABLES_RECEIPT="S";//收款单
+	
 	/**
 	 * 供应商编号的开始字母
 	 */
@@ -35,10 +54,40 @@ public interface Constants {
 	
 	final int CACHE_ONE_DAY_TIME=60*60*24;//缓存一天的时间
 	
+	/**
+	 * 销售合同前缀
+	 */
 	String SALES_CONTRACT_PREFIX="H_X";//销售合同前缀
-	
+	/**
+	 * 采购合同前缀
+	 */
 	String PURCHASE_CONTRACT_PREFIX="H_C";//采购合同前缀
+	/**
+	 * 销售订单前缀 XS_D
+	 */
+	String SALES_ORDER_PREFIX=SALES_PREFIX+"_D";//销售订单前缀
+	/**
+	 * 销售报价前缀 XS_B
+	 */
+	String SALES_QUOTATION_PREFIX=SALES_PREFIX+"_B";//销售报价前缀
+	/**
+	 * 采购询价前缀 CG_X
+	 */
+	String PURCHASE_INQUIRY_PREFIX=PURCHASE_PREFIX+"_X";//采购询价前缀
 	
-	String SALES_ORDER_PREFIX="XH_D";//销售订单前缀
+	/**
+	 * 采购订单前缀 CG_D
+	 */
+	String PURCHASE_ORDER_PREFIX=PURCHASE_PREFIX+"_D";//采购订单前缀
+	
+	/**
+	 * 库存入库前缀 KC_R
+	 */
+	String STOCK_STORAGE=STOCK_PREFIX+"_R";
+	
+	/**
+	 * 库存出库前缀 KC_C
+	 */
+	String STOCK_OUTSTORE=STOCK_PREFIX+"_C";//
 	
 }
