e4f23b925a0674798ad4b1c78e94eb990fdb0453
goodluck.sunlight@gmail.com <goodluck.sunlight@gmail.com@ab314482-b1d6-33f6-f488-e22901598cf5>
2014-04-15 02:51:04 +0000

    完成供应商业务

--- trunk/radius-invoicing/src/java/com/radius/invoicing/suppliermanage/service/SupplierService.java
+++ trunk/radius-invoicing/src/java/com/radius/invoicing/suppliermanage/service/SupplierService.java
@@ -1,6 +1,8 @@
 package com.radius.invoicing.suppliermanage.service;
 
 import com.radius.base.page.EasyuiSplitPager;
+import com.radius.base.utils.JsonUtils;
+import com.radius.invoicing.ibatis.model.LinkMan;
 import com.radius.invoicing.ibatis.model.Supplier;
 
 /**
@@ -19,5 +21,31 @@ public interface SupplierService {
 	 */
 	public EasyuiSplitPager<Supplier> getSupplierInfo(Supplier supplier)
 			throws Exception;
+
+	/**
+	 * 将供应商联系人添加到缓存中
+	 * @param key
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils addSupplierLinkMan2Memcached(String key, LinkMan linkMan);
+
+	/**
+	 * 移除供应商联系人信息从memcached中
+	 * @param key
+	 * @param isDelete true表示删除整个对象
+	 * @param grd
+	 * @return
+	 */
+	public JsonUtils removeSupplierLinkMan2Memcached(String key,
+			boolean isDelete, LinkMan linkMan);
+
+	/**
+	 * 保存供应商信息(如果存在则更新、否则添加)
+	 * @param key
+	 * @param supplier
+	 * @return
+	 */
+	public JsonUtils saveSupplierInfo(String key, Supplier supplier);
 
 }
