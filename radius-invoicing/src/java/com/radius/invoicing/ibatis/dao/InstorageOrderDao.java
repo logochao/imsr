1f1741e26a7e333c25979c161fa27e7aeb595038
goodluck.sunlight <goodluck.sunlight@gmail.com>
2014-04-25 12:39:15 +0000

    update 入库表信息Dao

--- radius-invoicing/src/java/com/radius/invoicing/ibatis/dao/InstorageOrderDao.java
+++ radius-invoicing/src/java/com/radius/invoicing/ibatis/dao/InstorageOrderDao.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import com.radius.invoicing.ibatis.model.InstorageOrder;
 
+/**
+ * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
+ * @version 创建时间：2013-12-8 下午12:53:04<br/>
+ * Copyright (c) 2013 by 陈波宁.<br/>
+ * 类说明
+ */
 public interface InstorageOrderDao {
 
 	/***
@@ -11,21 +17,22 @@ public interface InstorageOrderDao {
 	 * @param instoreId
 	 * @return
 	 */
-	public InstorageOrder getInstoreOrderById(String instoreId);
+	public InstorageOrder getInstorageOrderById(String instoreId);
 
 	/**
 	 * 通过条件查询入库信息
 	 * @param instoreOrder
 	 * @return
 	 */
-	public List<InstorageOrder> getInstoreOrderCondition(InstorageOrder instoreOrder);
+	public List<InstorageOrder> getInstorageOrderCondition(
+			InstorageOrder instoreOrder);
 
 	/**
 	 * 通过主键查询是否存在入库信息
 	 * @param instoreId
 	 * @return
 	 */
-	public Integer getInstoreOrderCountById(String instoreId);
+	public Integer getInstorageOrderCountById(String instoreId);
 
 	/**
 	 * 保存入库信息
@@ -44,13 +51,13 @@ public interface InstorageOrderDao {
 	 * @param instoreOrder
 	 * @return
 	 */
-	public boolean updateInstoreOrder(InstorageOrder instoreOrder);
+	public boolean updateInstorageOrder(InstorageOrder instoreOrder);
 
 	/**
 	 * 批量更新入库信息
 	 * @param list
 	 */
-	public void batchUpdateInstoreOrder(List<InstorageOrder> list);
+	public void batchUpdateInstorageOrder(List<InstorageOrder> list);
 
 	/**
 	 * 更新入库信息状态
@@ -64,19 +71,19 @@ public interface InstorageOrderDao {
 	 * @param instoreOrder
 	 * @return
 	 */
-	public boolean deleteInstoreOrderByCondition(InstorageOrder instoreOrder);
+	public boolean deleteInstorageOrderByCondition(InstorageOrder instoreOrder);
 
 	/**
 	 * 批量删除入库信息（通过主键）
 	 * @param list
 	 */
-	public void batchDeleteInstoreOrder(List<InstorageOrder> list);
+	public void batchDeleteInstorageOrder(List<InstorageOrder> list);
 
 	/**
 	 * 通过主键删除入库信息
 	 * @param instoreId
 	 * @return
 	 */
-	public boolean deleteInstoreOrderById(String instoreId);
+	public boolean deleteInstorageOrderById(String instoreId);
 
 }
