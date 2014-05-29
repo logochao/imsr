2c3d258b84e2b3a0ef3a335425e2a4cb07f73d39
goodluck.sunlight <goodluck.sunlight@gmail.com>
2014-05-29 03:11:37 +0000

    修改销售保教sqlmap、model、dao

--- radius-invoicing/src/java/com/radius/invoicing/ibatis/model/SupplierGoods.java
+++ radius-invoicing/src/java/com/radius/invoicing/ibatis/model/SupplierGoods.java
@@ -5,6 +5,10 @@ package com.radius.invoicing.ibatis.model;
 
 import java.util.Date;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import com.radius.base.utils.JsonDateSerializer;
+
 /**
  * @author <a href="mailto:goodluck.sunlight@gmail.com">陈波宁</a>
  * @version 创建时间：2013-12-8 下午12:53:04<br/>
@@ -121,7 +125,7 @@ public class SupplierGoods extends ExtInfo{
 	public void setUnit(Integer unit) {
 		this.unit = unit;
 	}
-
+	@JsonSerialize(using=JsonDateSerializer.class)
 	public Date getValidityDate() {
 		return validityDate;
 	}
